727f7758837a8a3876460ab2e39ae6553975b80b
Valerii Zakharov <infuzo1@gmail.com>
2022-01-29 17:47:27 +0200

    Add single order raw view.

--- ShopEngine/ShopEngine/Models/OrderModel.cs
+++ ShopEngine/ShopEngine/Models/OrderModel.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
