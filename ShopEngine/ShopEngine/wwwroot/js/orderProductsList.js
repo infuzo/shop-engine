7f9c3815400050c66cee8c4271cd50775e83d2e5
valerii.zakharov <valerii.zakharov@smartteksas.com>
2022-02-22 13:37:34 +0200

    Add editing of products count and displaying of total price.

--- ShopEngine/ShopEngine/wwwroot/js/orderProductsList.js
+++ ShopEngine/ShopEngine/wwwroot/js/orderProductsList.js
@@ -13,11 +13,14 @@
 	let buttonEditProductPrice = HTMLButtonElement;
 	let buttonRemoveProduct = HTMLButtonElement;
 
+	let optionsProductsInfo = new Map();
+
 	this.initialize = function () {
 		orderProductsSelectElement = document.getElementById(idSelectedOrderProductsList);
 		orderProductsSelectElement.addEventListener("change", onSelectedProductChange);
 
 		buttonEditProductCount = document.getElementById(idEditProductCount);
+		buttonEditProductCount.addEventListener("click", onButtonEditProductCountClick);
 		buttonEditProductPrice = document.getElementById(idEditProductPrice);
 		buttonRemoveProduct = document.getElementById(idRemoveProduct);
 
@@ -26,19 +29,20 @@
 
 	this.addProductToList = function (product) {
 		let option = document.createElement('option');
-		option.value = {
-			product: product,
-			count: 1,
-			overriddenPrice: -1 //price less than null for not-overridden price
-		};
+		optionsProductsInfo.set(option,
+			{
+				product: product,
+				count: 1,
+				overriddenPrice: -1 //price less than null for not-overridden price
+			});
 		updateOptionInnerHTML(option);
 		orderProductsSelectElement.appendChild(option);
 	}
 
 	function updateOptionInnerHTML(option) {
-		let productInfo = option.value;
+		let productInfo = optionsProductsInfo.get(option);
 		let price = productInfo.overriddenPrice < 0 ? productInfo.product.Price : productInfo.overriddenPrice;
-		option.innerHTML = productInfo.product.Name.substring(0, maxProductNameLength) + '    x' + productInfo.count + '    Cost: ' + price;
+		option.innerHTML = productInfo.product.Name.substring(0, maxProductNameLength) + '&nbsp;&nbsp;&nbsp;&nbsp;x' + productInfo.count + '&nbsp;&nbsp;&nbsp;&nbsp;Cost: ' + price + '&nbsp;&nbsp;&nbsp;&nbsp;Total: ' + (price * productInfo.count);
 	}
 
 	function updateProductsListButtonsVisibility() {
@@ -51,5 +55,23 @@
 	function onSelectedProductChange() {
 		orderProductsSelectElement.selectedIndex = orderProductsSelectElement.selectedIndex;
 		updateProductsListButtonsVisibility();
+	}
+
+	function onButtonEditProductCountClick() {
+		let option = orderProductsSelectElement.options[orderProductsSelectElement.selectedIndex];
+		let productInfo = optionsProductsInfo.get(option);
+		if (productInfo == undefined) {
+			console.error("Can't find product info for option " + option);
+			return;
+		}
+
+		let newCount = Number.parseInt(prompt("Enter new items count", productInfo.count));
+		if (isNaN(newCount) || newCount <= 0) {
+			alert("You entered incorrect number. Please enter integer less than zero.");
+			return;
+		}
+
+		productInfo.count = newCount;
+		updateOptionInnerHTML(option);
 	}
 }
